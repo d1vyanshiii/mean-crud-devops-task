a566ce54c7db320e6101254458406984c5cc7765
divyanshi <141664769+d1vyanshiii@users.noreply.github.com>
2025-08-23 20:49:48 +0000

    fix in baseURl

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://13.60.58.143:8080/api/tutorials';
+const baseUrl = 'http://13.60.58.143:5000/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
